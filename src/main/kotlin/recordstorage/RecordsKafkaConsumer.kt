04377c18a17f0943d17fc559f8f658c7ed842bb5
Russell Cheung <thechunk@gmail.com>
2019-05-13 12:39:27 +0800

    kafka: process messages as MessageRequest objects

--- src/main/kotlin/recordstorage/RecordsKafkaConsumer.kt
+++ src/main/kotlin/recordstorage/RecordsKafkaConsumer.kt
@@ -2,7 +2,7 @@ package recordstorage
 
 import common.constants.Kafka
 import common.engine.Consumer
-import common.entities.BudgetRecord
+import common.entities.MessageRequest
 import org.apache.kafka.clients.consumer.ConsumerRecord
 import org.slf4j.LoggerFactory
 import org.springframework.kafka.annotation.KafkaListener
@@ -10,12 +10,16 @@ import org.springframework.stereotype.Service
 import org.springframework.util.SerializationUtils
 
 @Service
-class RecordKafkaConsumer : Consumer {
+class RecordKafkaConsumer(val budgetService: BudgetService) : Consumer {
     val logger = LoggerFactory.getLogger(RecordKafkaConsumer::class.java)!!
 
     @KafkaListener(topics=[Kafka.RECORDS_TOPIC])
     override fun getMessage(record: ConsumerRecord<String, ByteArray>) {
-        val r = SerializationUtils.deserialize(record.value()) as BudgetRecord
+        val r = SerializationUtils.deserialize(record.value()) as MessageRequest
+        val tokens = r.tokens
+        if (tokens?.google != null) {
+            budgetService.processRecord(r.record, tokens.google)
+        }
         logger.debug("Receive: %s from %s".format(record.key(), record.topic()))
     }
 }
